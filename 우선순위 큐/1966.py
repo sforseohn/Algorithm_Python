f5fae10d5d12b04548c93c57f5e969fbc8fce467
sforseohn <sforseohn@ewhain.net>
2024-03-19 23:23:18 +0900

    Fix: 1966

--- 우선순위 큐/1966.py
+++ 우선순위 큐/1966.py
@@ -1,28 +1,30 @@
 # 프린터 큐
-import sys,collections
+import sys, collections
 input = sys.stdin.readline
 
-t = int(input())
-
-for _ in range(t):
-    n, m = map(int, input().split())
-    q = collections.deque(list(map(int, input().split())))
-    
+def solution(n, m, numbers):
+    queue = collections.deque(numbers)
     cnt = 0
+    numbers.sort()
     
-    while q:
-        big = max(q)
-        val = q.popleft()
-        m = (m - 1) 
-        
-        if val < big:
-            q.append(val)
-            if m < 0:
-                m = len(q) - 1
-        else:
+    while True:
+        if queue[0] == numbers[-1]:
             cnt += 1
-            if m < 0:
-                break
-    
-    print(cnt)
+            queue.popleft()
+            numbers.pop()
+            if m == 0:
+                return cnt
+        else:
+            queue.append(queue.popleft())  
+        m = len(queue) - 1 if m == 0 else m - 1
         
+        
+tc = int(input())
+
+for _ in range(tc):
+    n, m = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    
+    print(solution(n, m, numbers))
+
+    
